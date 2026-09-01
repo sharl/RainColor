07ee47fd5855ea79ab48848e4985343d3dca8d99
Sharl Morlaroll <sharl@hauN.org>
2026-09-01 17:06:03 +0900

    add amedas info

--- RainColor.py
+++ RainColor.py
@@ -22,12 +22,61 @@ import netifaces as netif
 import requests
 import schedule
 
-NAME = 'Yeelight Rain Color'
+NAME = 'Rain Color'
 
 INTERVAL = 5 * 60
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+D_TEMP = 100
+D_SNOW = -1
+WD = '静穏 北北東 北東 東北東 東 東南東 南東 南南東 南 南南西 南西 西南西 西 西北西 北西 北北西 北'.split()
+WEATHER_INFO = {
+    0: "晴",
+    1: "曇",
+    2: "煙霧",
+    3: "霧",
+    4: "降水またはしゅう雨性の降水",
+    5: "霧雨",
+    6: "着氷性の霧雨",
+    7: "雨",
+    8: "着氷性の雨",
+    9: "みぞれ",
+    10: "雪",
+    11: "凍雨",
+    12: "霧雪",
+    13: "しゅう雨または止み間のある雨",
+    14: "しゅう雪または止み間のある雪",
+    15: "ひょう",
+    16: "雷",
+    30: "天気不明",
+    31: "欠測",
+}
+
+# AQC (Automatic Quality Control) 識別符号
+# https://www.data.jma.go.jp/stats/data/mdrr/man/remark.html
+# https://www.data.jma.go.jp/suishin/shiyou/pdf/no13301
+# 0 正常
+# 1 準正常 (やや疑わしい)
+# 2 非常に疑わしい
+# 3 利用に適さない
+# 4 観測値は期間内で資料数が不足している
+# 5 点検又は計画休止のため欠測
+# 6 障害のため欠測
+# 7 この要素の観測はしていない
+AQC_INFO = {
+    0: '',
+    1: ')',
+    2: '#',
+    3: '#',
+    4: ']',
+    5: '休止中',
+    6: '✕',
+    None: '　',
+}
+ずんだもん = [3, 22]
+四国めたん = [2, 36]
+
 PreferredAppMode = {
     'Light': 0,
     'Dark': 1,
@@ -94,6 +143,8 @@ class taskTray:
         self.config = {}
         self.bulbs = []
 
+        # 最初に定義された amedas code
+        self.default = None
         self.readConf(False)
 
         # 初期アイコン
@@ -133,6 +184,8 @@ class taskTray:
                         k, v = ll.split('=')
                         self.config[section][k] = float(v)
                     code, name, _ = getNearAmedas(self.config[section]['lat'], self.config[section]['lon'])
+                    if not self.default:
+                        self.default = code
                     self.config[section]['code'] = code
                 elif key == 'bulb':
                     for bulb_ip in self.config[section]['bulb'].split():
@@ -156,7 +209,8 @@ class taskTray:
             if not self.config[name].get('code'):
                 return
 
-            r, g, b = self.getRGB(name, self.config[name])
+            r, g, b = self.getRGB(name)
+            lines += self.config[name].get('lines', [])
             rgb = f'{r} {g} {b}'
 
             # bulbs operation (Yeelight)
@@ -270,11 +324,44 @@ class taskTray:
 
         self.app.run()
 
-    def getRGB(self, name, data):
-        # print('getRGB', data)
-        code = data['code']
+    def daytime(self, speaker):
+        now = dt.datetime.now(dt.timezone(dt.timedelta(hours=9)))
+        if now.hour <= 5:
+            return speaker[1]
+        return speaker[0]
+
+    def vvox_temp(self, name):
+        if self.config[name].get('vvox').lower() != 'on':
+            return
+
+        _name = '' if self.config[name].get('code') == self.default else f'{name}が'
+        temp = self.config[name].get('temp')
+        pm = ''
+        if temp < 0:
+            temp = -temp
+            pm = 'マイナス'
+        vvox(f"{_name}{pm}{str(temp).replace('.0', '')}度になったのだ", speaker=self.daytime(ずんだもん))
+
+    def vvox_snow(self, name, plus):
+        if self.config[name].get('vvox').lower() != 'on':
+            return
+
+        _name = '' if self.config[name].get('code') == self.default else f'{name}が'
+        _plus = '増えた' if plus else 'なった'
+        vvox(f'{_name}{self.snow}センチに{_plus}わ', speaker=self.daytime(四国めたん))
+
+    def vvox_weather(self, name):
+        if self.config[name].get('vvox').lower() != 'on':
+            return
+
+        _name = '' if self.config[name].get('code') == self.default else f'{self.name}は'
+        vvox(f'{_name}{self.config[name].get('weather', '不明')}なのだ', speaker=self.daytime(ずんだもん))
+
+    def getRGB(self, name):
+        # print('getRGB', self.config[name])
+        code = self.config[name]['code']
         rainsnow = False
-        base = data['location'].split('?')
+        base = self.config[name]['location'].split('?')
         now = dt.datetime.now(dt.timezone(dt.timedelta(hours=9))) - dt.timedelta(minutes=10)
         yyyymmdd = now.strftime('%Y%m%d')
         HH = now.strftime('%H')
@@ -284,12 +371,75 @@ class taskTray:
             with requests.get(url, timeout=10) as r:
                 data = r.json()
                 base_key = f'{yyyymmdd}{HH}0000'        # 積雪は1時間毎    pass
+                last_key = list(data.keys())[-1]
+                _vars = data[base_key]
+                for k in data[last_key]:
+                    _vars[k] = data[last_key][k]
                 cm, aqc = data[base_key].get('snow', [None, None])
                 # 0: 正常 1: 准正常
                 if cm is not None and (aqc != 0 or aqc != 1):
                     rainsnow = True
+                h = last_key[8:10]
+                if h == '00':
+                    h = '24'
+                m = last_key[10:12]
+                lines = [
+                    f'{name} {h}:{m}',
+                ]
+                weather = None
+                temp = D_TEMP
+                snow = D_SNOW
+                for x in [
+                        '天気 weather -',
+                        '気温 temp 度',
+                        '降水 precipitation1h mm/h',
+                        '風向 windDirection -',
+                        '風速 wind m/s',
+                        '積雪 snow cm',
+                        '降雪 snow1h cm/h',
+                        '湿度 humidity %',
+                        '気圧 pressure hPa',
+                ]:
+                    t, k, u = x.split()
+                    if k in _vars:
+                        v, aqc = _vars[k]
+                        # print(k, [v, aqc])
+                        # 0: 正常 1: 准正常
+                        if aqc != 0 and aqc != 1:
+                            continue
+
+                        if isinstance(v, float):
+                            if v == int(v):
+                                v = int(v)
+
+                        if k == 'weather':
+                            weather = WEATHER_INFO[v]
+                            if weather != self.config[name].get('weather'):
+                                self.config[name]['weather'] = weather
+                                self.vvox_weather(name)
+                        elif k == 'temp':
+                            temp = v
+                            if int(temp) != int(self.config[name].get('temp', D_TEMP)):
+                                self.config[name]['temp'] = temp
+                                self.vvox_temp(name)
+                        elif k == 'snow':
+                            snow = v
+                            if snow is not None and snow != self.config[name].get('snow'):
+                                plus = snow > self.config[name].get('snow') and self.config[name].get('snow') != D_SNOW
+                                self.config[name]['snow'] = snow
+                                self.vvox_snow(name, plus)
+
+                        if k == 'windDirection':
+                            lines.append(f'{t} {WD[v]}')
+                        elif k == 'weather':
+                            lines.append(f'{t} {WEATHER_INFO[v]}')
+                        else:
+                            lines.append(f'{t} {v}{u}')
+                self.config[name]['lines'] = lines
         except Exception as e:
             logger.warning(e)
+        finally:
+            pass
 
         self.config[name]['rainsnow'] = rainsnow
         base_url = f'{base[0]}{"rainsnow/" if rainsnow else ""}?{base[1]}'
