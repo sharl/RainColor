397bd3a50d278f6b0830b404ad29f260c4ea6ca4
Sharl Morlaroll <sharl@hauN.org>
2026-09-03 13:25:09 +0900

    import from getLog

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
 import os
 import sys
+
+
+def getLog(APP_NAME: str, name: str) -> Path:
+    logdir = Path(os.environ.get('XDG_STATE_HOME', Path.home() / '.local/state') / Path(APP_NAME))
+    os.makedirs(logdir, exist_ok=True)
+    return logdir / Path(name)
 
 
 def resource_path(path):
